09862a92e8343dd6a80dee544dcb1e1b26ad0e05
Ravs10 <run4ravish@gmail.com>
2026-09-12 21:41:05 +0530

    Add RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,4 @@
+    export const metadata = { title: "Lorem Pro" };
+    export default function RootLayout({ children }: { children: React.ReactNode }) {
+      return <html><body style={{margin:0, fontFamily:'sans-serif'}}>{children}</body></html>
+    }
