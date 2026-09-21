198212807f6fb4f58ca1ad1f89ba552514943f5c
Ravs10 <run4ravish@gmail.com>
2026-09-21 15:28:58 +0530

    Add fake data generator URL to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
+    {
+  url: 'https://lorem-pro-tool.vercel.app/fake-data-generator',
+  lastModified: new Date(),
+  changeFrequency: 'weekly',
+  priority: 0.8,
+},
   ]
 }
