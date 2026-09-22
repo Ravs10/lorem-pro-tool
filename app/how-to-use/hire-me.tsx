ee0bd1ccb215da2d80b0596c41f8b6d9a91f847d
Ravs10 <run4ravish@gmail.com>
2026-09-22 15:02:36 +0530

    Delete app/how-to-use/hire-me.tsx

--- app/how-to-use/hire-me.tsx
+++
@@ -1,8 +0,0 @@
-export default function HireMe() {
-  return (
-    <div style={{padding:"40px", maxWidth:"800px", margin:"auto"}}>
-      <h1>Contact</h1>
-      <p>Contact page content here...</p>
-    </div>
-  );
-}
