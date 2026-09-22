22ccee77231562c4aefee928b1a18aef86000ab2
Ravs10 <run4ravish@gmail.com>
2026-09-22 15:09:25 +0530

    Add HowToUse component for Hire Me page

---
+++ app/how-to-use/page.tsx
@@ -0,0 +1,8 @@
+export default function HowToUse() {
+  return (
+    <div style={{padding:"40px", maxWidth:"800px", margin:"auto"}}>
+      <h1>Hire Me</h1>
+      <p>Hire Me page content...</p>
+    </div>
+  );
+}
