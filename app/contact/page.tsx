bfa157210f1aa678cc074968f984b4bfd0aba13c
Ravs10 <run4ravish@gmail.com>
2026-09-22 14:48:54 +0530

    Create Contact page component

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div style={{padding:"40px", maxWidth:"800px", margin:"auto"}}>
+      <h1>Contact</h1>
+      <p>Contact page content here...</p>
+    </div>
+  );
+}
