dee6b37415ce00dcf7ec76145b9da7984725bf79
Ravs10 <run4ravish@gmail.com>
2026-09-27 03:14:24 +0530

    Enhance RootLayout with metadata and structure
    Added metadata for title and description to RootLayout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,25 @@
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-...
-<body>
-  <Header />
-  {children}
-  <Footer />
-</body>
+import type { Metadata } from "next";
+import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export const metadata: Metadata = {
+  title: "Lorem Pro Tools",
+  description: "All in one tools",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
