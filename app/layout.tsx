2471ef550a5a3b418ab345ec27a525981c2e051a
Ravs10 <run4ravish@gmail.com>
2026-09-27 02:18:51 +0530

    Replace Navbar with Header and add Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,8 @@
-import "./globals.css";
-import Navbar from "./components/Navbar";
-//import Footer from "./components/Footer";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, background: "#f8f9ff" }}>
-        <Navbar />
-        {children}
-        {/*<Footer />*/} 
-      </body>
-    </html>
-  );
-}
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+...
+<body>
+  <Header />
+  {children}
+  <Footer />
+</body>
